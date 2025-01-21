4b6ddae09cdb685ab58b717c09af164acabc419a
Sriram Sai Ganesh <sriram@pitzer-rw02.ten.osc.edu>
2025-01-21 11:50:36 -0500

    read datasets

---
+++ read_tweets.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from tqdm import tqdm
+
+DATA_FOLDER = 'data'
+
+# load tweets from sanders dataset
+def load_sanders_tweets(filename: str) -> pd.DataFrame:
+    filepath = f"{DATA_FOLDER}/{filename}"
+    df = pd.read_csv(filepath)
+    df.drop(columns=['TweetId', 'Sentiment'], inplace=True)
+    df.dropna(inplace=True)
+    df['TweetDate'] = pd.to_datetime(df['TweetDate'])
+    return df
+
+# load tweets from celebrities
+def load_celebrity_tweets(filename: str) -> pd.DataFrame:
+    column_names = ['user', 'tweet', 'sentiment_label']
+    df = pd.read_csv(f'{DATA_FOLDER}/{filename}', header=None, names=column_names)
+    df.dropna(inplace=True)
+    return df
+
+def load_cikm_tweets(filename: str) -> pd.DataFrame:
+    # helper to process a single file line
+    def process_line(line):
+        parts = line.strip().split('\t')
+        if len(parts) != 4:
+            return None
+        user_id, tweet_id, tweet, created_at = parts
+        # return {'Tweet': tweet, 'CreatedAt': created_at}
+        return (tweet, created_at)
+    
+    # read file and process each line:
+    file_lines = open(f'{DATA_FOLDER}/{filename}').readlines()
+    results = []
+    for line in tqdm(file_lines):
+        result = process_line(line)
+        if result:results.append(result)
+    
+    df = pd.DataFrame(results, columns=['Tweet', 'CreatedAt'])
+    # read created at column as datetime, and drop any row with invalid values:
+    df['CreatedAt'] = pd.to_datetime(df['CreatedAt'], errors='coerce')
+    df.dropna(inplace=True)
+    return df
+
+
+def debug():
+    df = load_celebrity_tweets('celebrity_tweets_results.csv')
+    print(f'Celebrity tweets: {len(df)} loaded.\n{df.head()}')
+
+    df = load_sanders_tweets('sanders_corpus.csv')
+    print(f'Sanders tweets: {len(df)} loaded.\n{df.head()}')
+
+    df = load_cikm_tweets('cikm_2010_tweets.txt')
+    print(f'CIKM tweets: {len(df)} loaded.\n{df.head()}')
+
+if __name__ == '__main__':
+    debug()
