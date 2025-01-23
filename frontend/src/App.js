670984ba3b1e52474d60740354646e8bcff08dd4
Kevin Lewis <kevindlewis23@gmail.com>
2025-01-23 09:38:58 -0500

    Starting react app

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <img src={logo} className="App-logo" alt="logo" />
+        <span>EchoX</span>
+      </header>
+      <div className="Body">
+
+        <span className = "question">
+          What topic would you like to explore?
+        </span>
+        <span className = "input-group">
+          <input id="topic" className="input" placeholder = "Twitter, e.g."></input>
+          <button className = "btn">Search</button>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
