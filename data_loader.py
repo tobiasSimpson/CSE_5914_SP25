26ce890c3491810fa24360f8c7274655947f5c64
tobiasSimpson <93829921+tobiasSimpson@users.noreply.github.com>
2025-02-11 13:51:29 -0500

    Combine data into json

--- data_loader.py
+++ data_loader.py
@@ -1,79 +1,30 @@
 import csv
 import json
+import pandas as pd
 from tqdm import tqdm
 
 DATA_FOLDER = 'data'
 
 # load tweets from sanders dataset
-def load_sanders_tweets(filename: str):
-    # Create tweet list to add to
-    tweets = list()
-
+def load_sanders_tweets(filename: str, silent: bool) -> pd.DataFrame:
     # Create file names for csv and json files
     csvFilePath = f"{DATA_FOLDER}/{filename}.csv"
     jsonFilePath = f"{DATA_FOLDER}/{filename}.json"
 
     # Open the csv file
-    with open(csvFilePath, encoding='utf-8') as file:
-        # Read csv as dict
-        csvReader = csv.DictReader(file)
+    column_names = ["topic", "sentiment", "tweet_id", "date", "text"]
+    tweets = pd.read_csv(csvFilePath, header=None, names=column_names)
+    tweets.dropna(inplace=True)
 
-        # Process each row as a tweet
-        for row in tqdm(csvReader):
-            # Create dict for tweet
-            tweet = dict()
+    return tweets
 
-            # Add each tweet element to dict
-            tweet['topic'] = row['Topic']
-            tweet['sentiment'] = row['Sentiment']
-            tweet['ID'] = row['TweetId']
-            tweet['date'] = row['TweetDate']
-            tweet['text'] = row['TweetText']
+def load_celebrity_tweets(filename: str) -> pd.DataFrame:
+    column_names = ['user', 'sentiment', 'text']
+    tweets = pd.read_csv(f'{DATA_FOLDER}/{filename}.csv', header=None, names=column_names)
+    tweets.dropna(inplace=True)
+    return tweets
 
-            # Add tweet to list of tweets
-            tweets.append(tweet)
-    # Clear json file
-    open(jsonFilePath, 'w').close()
-    # Open json file
-    with open(jsonFilePath, "w") as file:
-        # Dump tweets into json
-        file.write(json.dumps(tweets))
-
-
-# load tweets from celebrities
-def load_celebrity_tweets(filename: str):
-    # Create tweet list to add to
-    tweets = list()
-
-    # Create file names for csv and json files
-    csvFilePath = f"{DATA_FOLDER}/{filename}.csv"
-    jsonFilePath = f"{DATA_FOLDER}/{filename}.json"
-
-    # Open the csv file
-    with open(csvFilePath, encoding='utf-8') as file:
-        # Read csv as dict
-        csvReader = csv.reader(file)
-
-        # Process each row as a tweet
-        for row in tqdm(csvReader):
-            # Create dict for tweet
-            tweet = dict()
-
-            # Add each tweet element to dict
-            tweet['author'] = row[0]
-            tweet['sentiment'] = row[2]
-            tweet['text'] = row[1]
-
-            # Add tweet to list of tweets
-            tweets.append(tweet)
-    # Clear json file
-    open(jsonFilePath, 'w').close()
-    # Open json file
-    with open(jsonFilePath, "w") as file:
-        # Dump tweets into json
-        file.write(json.dumps(tweets))
-
-def load_cikm_tweets(filename: str):
+def load_cikm_tweets(filename: str, silent: bool):
     # helper to process a single file line
     def process_line(line):
         parts = line.strip().split('\t')
@@ -114,9 +65,12 @@ def load_cikm_tweets(filename: str):
         file.write(json.dumps(tweets))
 
 def load_data():
-    load_sanders_tweets('sanders_corpus')
-    load_celebrity_tweets('celebrity_tweets_results')
-    load_cikm_tweets('cikm_2010_tweets')
+    sander_tweets = load_sanders_tweets('sanders_corpus', False)
+    celeb_tweets = load_celebrity_tweets('celebrity_tweets_results')
+    #load_cikm_tweets('cikm_2010_tweets', False)
+    data = pd.concat([sander_tweets, celeb_tweets], ignore_index=True, sort=False)
+    print(data.head())
+    data.to_json(f"{DATA_FOLDER}/data.json", orient='records')
 
 if __name__ == '__main__':
     load_data()
