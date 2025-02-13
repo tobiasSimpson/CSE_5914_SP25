109a2c59f0fa2652c5fc04f608e91c39c91faee3
Sriram Sai Ganesh <sriram@pitzer-rw02.ten.osc.edu>
2025-02-13 12:06:17 -0500

    reading tweets standardized

--- read_tweets.py
+++ read_tweets.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 DATA_FOLDER = 'data'
 
 # load tweets from sanders dataset
-def load_sanders_tweets(filename: str) -> pd.DataFrame:
+def load_sanders_tweets(filename: str = 'sanders_corpus.csv') -> pd.DataFrame:
     filepath = f"{DATA_FOLDER}/{filename}"
     df = pd.read_csv(filepath)
     df.drop(columns=['TweetId', 'Sentiment'], inplace=True)
@@ -16,10 +16,19 @@ def load_sanders_tweets(filename: str) -> pd.DataFrame:
 def load_celebrity_tweets(filename: str = 'celebrity_tweets.csv') -> pd.DataFrame:
     column_names = ['user', 'tweet', 'sentiment_label']
     df = pd.read_csv(f'{DATA_FOLDER}/{filename}', header=None, names=column_names)
+    # drop sentiment column:
+    df.drop(columns=['sentiment_label'], inplace=True)
     df.dropna(inplace=True)
     return df
 
-def load_cikm_tweets(filename: str) -> pd.DataFrame:
+# load trump tweets dataset
+def load_trump_tweets(filename: str) -> pd.DataFrame:
+    column_names = ['text', 'favorites', 'retweets', 'date']
+    tweets = pd.read_csv(f'{DATA_FOLDER}/{filename}.csv', usecols=column_names)
+    tweets.dropna(inplace=True)
+    return tweets
+
+def load_cikm_tweets(filename: str = 'cikm_2010_tweets.txt') -> pd.DataFrame:
     # helper to process a single file line
     def process_line(line):
         parts = line.strip().split('\t')
@@ -44,13 +53,14 @@ def load_cikm_tweets(filename: str) -> pd.DataFrame:
 
 
 def debug():
-    df = load_celebrity_tweets('celebrity_tweets.csv')
+    print(f'testing tweet file loading...')
+    df = load_celebrity_tweets()
     print(f'Celebrity tweets: {len(df)} loaded.\n{df.head()}')
 
-    df = load_sanders_tweets('sanders_corpus.csv')
+    df = load_sanders_tweets()
     print(f'Sanders tweets: {len(df)} loaded.\n{df.head()}')
 
-    df = load_cikm_tweets('cikm_2010_tweets.txt')
+    df = load_cikm_tweets()
     print(f'CIKM tweets: {len(df)} loaded.\n{df.head()}')
 
 if __name__ == '__main__':
