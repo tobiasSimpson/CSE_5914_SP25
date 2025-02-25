c22ac768898c8d64ec89d1fd9d3f39d0435e20b7
Kevin Lewis <kevindlewis23@gmail.com>
2025-02-25 13:42:20 -0500

    Some linkage between front and backend

--- frontend/src/FrontPage.js
+++ frontend/src/FrontPage.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"; 
 import Button from "react-bootstrap/Button";
 import "./FrontPage.css";
+import HomePage from "./HomePage";
 import logo from "./logo.png"; 
 
 function FrontPage () {
@@ -9,16 +10,16 @@ function FrontPage () {
 
   return (
     <div className="front-page">
-      <div className="overlay">
-        <img src={logo} alt="EchoX Logo" className="logo" />
-        <h1 className="title">Welcome to EchoX</h1>
-        <p className="tagline">
-          Discover Twitter trends and analysis over time.
-        </p>
-        <Button variant="primary" className="start-btn" onClick={() => navigate("/home")}>
-          Get Started
-        </Button>
-      </div>
+        <div id="front-overlay" className="overlay">
+          <img src={logo} alt="EchoX Logo" className="logo" />
+          <h1 className="title">Welcome to EchoX</h1>
+          <p className="tagline">
+            Discover Twitter trends and analysis over time.
+          </p>
+          <Button variant="primary" className="start-btn" onClick={()=>navigate("/home")}>
+            Get Started
+          </Button>
+        </div> 
     </div>
   );
 };
---
+++ make_rag_request.py
@@ -0,0 +1,32 @@
+import weaviate
+from weaviate.classes.init import Auth
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+wcd_url = os.environ["WCD_URL"]
+wcd_api_key = os.environ["WCD_API_KEY"]
+cohere_api_key = os.environ["COHERE_API_KEY"]
+
+client = weaviate.connect_to_weaviate_cloud(
+    cluster_url=wcd_url,
+    auth_credentials=Auth.api_key(wcd_api_key),
+    headers={"X-Cohere-Api-Key": cohere_api_key}
+)
+
+questions = client.collections.get("tweets")
+
+def make_request(query):
+    response = questions.generate.near_text(
+        query=query,
+        limit=10,   # can't be too high
+        grouped_task="Create a concise twitter-style post including the information in the following tweets. Do NOT include any extraneous text, ONLY directly generate the resulting tweet.",
+    )
+
+    text = [obj.properties["text"] for obj in response.objects]
+    generated_text = response.generated.strip('"')
+    return {'text': text, 'generated': generated_text}
+
+
+def close():
+    client.close()
